8900967aa2b24008d0df7dd040774a490769b06b
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2024-05-25 09:18:13 +0430

    some changes on header as test commit done

--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -48,11 +48,11 @@ const Header = (props: IHeaderProps) => {
 
   return (
     <div id="app-header">
-      {renderDevRibbon()}
+      {/* {renderDevRibbon()} */}
       <LoadingBar className="loading-bar" />
       <Navbar data-cy="navbar" light expand="md" fixed="top" className="bg-light">
         <NavbarToggler aria-label="Menu" onClick={toggleMenu} />
-        <Brand />
+        {/* <Brand /> */}
         <Collapse isOpen={menuOpen} navbar>
           <Nav id="header-tabs" className="ms-auto" navbar>
             <Home />
